f79dcc5d62481bebbc7c71d47cdb1b06de06f783
wangalei <244341474@qq.com>
2017-03-29 00:10:15 +0800

    SHA_1加密方式
    1、新增工具目录文件夹。
    2、增加SHA_1加密工具类

---
+++ app/app/src/main/java/com/example/wangalei/myapplication/Utils/Utils_SHA_1.java
@@ -0,0 +1,35 @@
+package com.example.wangalei.myapplication.Utils;
+
+import java.security.MessageDigest;
+
+/**
+ * Created by wangalei on 2017/3/29.
+ * 调用时传入需要加密字符串，返回SHA_1加密后的字符串
+ */
+public class Utils_SHA_1 {
+    public static String getSha1(String str) {
+        if (str == null || str.length() == 0) {
+            return null;
+        }
+        char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f' };
+        try {
+            MessageDigest mdTemp = MessageDigest.getInstance("SHA1");
+            mdTemp.update(str.getBytes("UTF-8"));
+
+            byte[] md = mdTemp.digest();
+            int j = md.length;
+            char buf[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                buf[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                buf[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(buf);
+        } catch (Exception e) {
+            // TODO: handle exception
+            return null;
+        }
+    }
+}

f36db2f6f8103aeeb5be137398820357ed83cf99
violinX <244341474@qq.com>
2017-05-03 17:53:13 +0800

    相机三方调用，未完成
    相机三方调用，未完成

--- app/app/src/main/java/com/example/wangalei/myapplication/Activity/ShowDemoTakePhotoActivity.java
+++ app/app/src/main/java/com/example/wangalei/myapplication/Activity/ShowDemoTakePhotoActivity.java
@@ -4,12 +4,88 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.wangalei.myapplication.R;
+import com.jph.takephoto.app.TakePhoto;
+import com.jph.takephoto.app.TakePhotoActivity;
+import com.jph.takephoto.model.InvokeParam;
+import com.jph.takephoto.model.TResult;
+import com.jph.takephoto.permission.InvokeListener;
+import com.jph.takephoto.permission.PermissionManager;
 
-public class ShowDemoTakePhotoActivity extends AppCompatActivity,TakePhotoActivity,TakePhotoFragmentActivity,TakePhotoFragment {
-
+public class ShowDemoTakePhotoActivity extends AppCompatActivity  implements TakePhoto.TakeResultListener,InvokeListener {
+    private static final String TAG = TakePhotoActivity.class.getName();
+    private TakePhoto takePhoto;
+    private InvokeParam invokeParam;
     @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getTakePhoto().onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+    }
+/*    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_demo_take_photo);
+    }*/
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        getTakePhoto().onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        getTakePhoto().onActivityResult(requestCode, resultCode, data);
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        TPermissionType type=PermissionManager.onRequestPermissionsResult(requestCode,permissions,grantResults);
+        PermissionManager.handlePermissionsResult(this,type,invokeParam,this);
+    }
+
+    /**
+     *  获取TakePhoto实例
+     * @return
+     */
+    public TakePhoto getTakePhoto(){
+        if (takePhoto==null){
+            takePhoto= (TakePhoto) TakePhotoInvocationHandler.of(this).bind(new TakePhotoImpl(this,this));
+        }
+        return takePhoto;
+    }
+    @Override
+    public void takeSuccess(TResult result) {
+        Log.i(TAG,"takeSuccess：" + result.getImage().getCompressPath());
+    }
+    @Override
+    public void takeFail(TResult result,String msg) {
+        Log.i(TAG, "takeFail:" + msg);
+    }
+
+    @Override
+    public void takeSuccess(TResult result) {
+
+    }
+
+    @Override
+    public void takeCancel() {
+        Log.i(TAG, getResources().getString(R.string.msg_operation_canceled));
+    }
+
+    @Override
+    public TPermissionType invoke(InvokeParam invokeParam) {
+        TPermissionType type=PermissionManager.checkPermission(TContextWrap.of(this),invokeParam.getMethod());
+        if(TPermissionType.WAIT.equals(type)){
+            this.invokeParam=invokeParam;
+        }
+        return type;
+    }
+
+    @Override
+    public PermissionManager.TPermissionType invoke(InvokeParam invokeParam) {
+        return null;
     }
 }
+
+
+

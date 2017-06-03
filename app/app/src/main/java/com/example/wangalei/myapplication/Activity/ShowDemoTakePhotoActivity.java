46319891c84c1f729b8f6f1b2f685ef69acb19a6
violinX <244341474@qq.com>
2017-06-03 17:57:27 +0800

    takephoto
    调试未完成

--- app/app/src/main/java/com/example/wangalei/myapplication/Activity/ShowDemoTakePhotoActivity.java
+++ app/app/src/main/java/com/example/wangalei/myapplication/Activity/ShowDemoTakePhotoActivity.java
@@ -1,30 +1,41 @@
 package com.example.wangalei.myapplication.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.example.wangalei.myapplication.R;
+import com.example.wangalei.myapplication.Utils.T;
 import com.jph.takephoto.app.TakePhoto;
 import com.jph.takephoto.app.TakePhotoActivity;
+import com.jph.takephoto.app.TakePhotoImpl;
 import com.jph.takephoto.model.InvokeParam;
+import com.jph.takephoto.model.TContextWrap;
 import com.jph.takephoto.model.TResult;
 import com.jph.takephoto.permission.InvokeListener;
 import com.jph.takephoto.permission.PermissionManager;
+import com.jph.takephoto.permission.PermissionManager.TPermissionType;
+import com.jph.takephoto.permission.TakePhotoInvocationHandler;
 
-public class ShowDemoTakePhotoActivity extends AppCompatActivity  implements TakePhoto.TakeResultListener,InvokeListener {
-    private static final String TAG = TakePhotoActivity.class.getName();
+
+public class ShowDemoTakePhotoActivity extends TakePhotoActivity  implements TakePhoto.TakeResultListener,InvokeListener,OnClickListener{
+    private static final String TAG = ShowDemoTakePhotoActivity.class.getName();
     private TakePhoto takePhoto;
     private InvokeParam invokeParam;
+
+    private Button bt_take_photo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getTakePhoto().onCreate(savedInstanceState);
-        super.onCreate(savedInstanceState);
-    }
-/*    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_demo_take_photo);
-    }*/
+        super.onCreate(savedInstanceState);
+
+        bt_take_photo= (Button) findViewById(R.id.bt_take_photo);
+        bt_take_photo.setOnClickListener(this);
+    }
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         getTakePhoto().onSaveInstanceState(outState);
@@ -39,6 +50,7 @@ public class ShowDemoTakePhotoActivity extends AppCompatActivity  implements Tak
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //以下代码为处理Android6.0、7.0动态权限所需
         TPermissionType type=PermissionManager.onRequestPermissionsResult(requestCode,permissions,grantResults);
         PermissionManager.handlePermissionsResult(this,type,invokeParam,this);
     }
@@ -61,12 +73,6 @@ public class ShowDemoTakePhotoActivity extends AppCompatActivity  implements Tak
     public void takeFail(TResult result,String msg) {
         Log.i(TAG, "takeFail:" + msg);
     }
-
-    @Override
-    public void takeSuccess(TResult result) {
-
-    }
-
     @Override
     public void takeCancel() {
         Log.i(TAG, getResources().getString(R.string.msg_operation_canceled));
@@ -82,10 +88,13 @@ public class ShowDemoTakePhotoActivity extends AppCompatActivity  implements Tak
     }
 
     @Override
-    public PermissionManager.TPermissionType invoke(InvokeParam invokeParam) {
-        return null;
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bt_take_photo:
+                T.showShort(this,"点击");
+                takePhoto=null;
+                getTakePhoto().onPickFromCapture();
+                break;
+        }
     }
 }
-
-
-

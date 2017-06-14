03cd50710ec92f67eeb9ea670885200b164fb781
violinX <244341474@qq.com>
2017-06-14 18:02:11 +0800

    recycler基础示例调试完成
    recycler基础示例调试完成

---
+++ app/app/src/main/java/com/example/wangalei/myapplication/Activity/OnItemClickLitener.java
@@ -0,0 +1,11 @@
+package com.example.wangalei.myapplication.Activity;
+
+import android.view.View;
+
+/**
+ * Created by JD on 2017/6/14.
+ */
+public interface OnItemClickLitener {
+    void onItemClick(View view, int position);
+    void onItemLongClick(View view , int position);
+}
--- app/app/src/main/java/com/example/wangalei/myapplication/Activity/ShowDemoRecyclerViewActivity.java
+++ app/app/src/main/java/com/example/wangalei/myapplication/Activity/ShowDemoRecyclerViewActivity.java
@@ -2,23 +2,28 @@ package com.example.wangalei.myapplication.Activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.wangalei.myapplication.Adapter.ShowDemoDividerGridItemDecoration;
 import com.example.wangalei.myapplication.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShowDemoRecyclerViewActivity extends AppCompatActivity {
+public class ShowDemoRecyclerViewActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RecyclerView mRecyclerView;
     private List<String> mDatas;
     private HomeAdapter mAdapter;
+    private Button add,remove;
 
 
     @Override
@@ -28,12 +33,50 @@ public class ShowDemoRecyclerViewActivity extends AppCompatActivity {
 
         initData();
         mRecyclerView = (RecyclerView) findViewById(R.id.id_recyclerview);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //mRecyclerView.setLayoutManager(new LinearLayoutManager(this));          //listview样式
+
+        //mRecyclerView.setLayoutManager(new GridLayoutManager(this,4));          //gridview样式
+        //横向纵向滑动控制 第一个参数是用来设定行列的个数，第二个参数是用来空置纵向排列还是横向
+        mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.VERTICAL));
+        //mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(4,StaggeredGridLayoutManager.HORIZONTAL));
+
+        //瀑布流
+
         mRecyclerView.setAdapter(mAdapter = new HomeAdapter());
+        //listview 分割线 样式
+        //mRecyclerView.addItemDecoration(new ShowDemoDividerItemDecoration(this,
+        //        ShowDemoDividerItemDecoration.VERTICAL_LIST));
+        mRecyclerView.addItemDecoration(new ShowDemoDividerGridItemDecoration(this));
+
+        // 设置item动画
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        mAdapter.setOnItemClickLitener(new OnItemClickLitener()
+        {
+
+            @Override
+            public void onItemClick(View view, int position)
+            {
+                Toast.makeText(ShowDemoRecyclerViewActivity.this, position + " click",
+                        Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemLongClick(View view, int position)
+            {
+                Toast.makeText(ShowDemoRecyclerViewActivity.this, position + " long click",
+                        Toast.LENGTH_SHORT).show();
+                mAdapter.removeData(position);
+            }
+        });
     }
 
     protected void initData()
     {
+        add=(Button)findViewById(R.id.add);
+        remove=(Button)findViewById(R.id.remove);
+        add.setOnClickListener(this);
+        remove.setOnClickListener(this);
         mDatas = new ArrayList<String>();
         for (int i = 'A'; i < 'z'; i++)
         {
@@ -41,8 +84,31 @@ public class ShowDemoRecyclerViewActivity extends AppCompatActivity {
         }
     }
 
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.add:
+                mAdapter.addData(1);
+                break;
+            case R.id.remove:
+                mAdapter.removeData(1);
+                break;
+        }
+    }
+
     class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder>
     {
+        private OnItemClickLitener mOnItemClickLitener;
+
+        public void setOnItemClickLitener(OnItemClickLitener mOnItemClickLitener)
+        {
+            this.mOnItemClickLitener = mOnItemClickLitener;
+        }
+
+
 
         @Override
         public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
@@ -54,9 +120,34 @@ public class ShowDemoRecyclerViewActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onBindViewHolder(MyViewHolder holder, int position)
+        public void onBindViewHolder(final MyViewHolder holder, int position)
         {
             holder.tv.setText(mDatas.get(position));
+
+            // 如果设置了回调，则设置点击事件
+            if (mOnItemClickLitener != null)
+            {
+                holder.itemView.setOnClickListener(new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        int pos = holder.getLayoutPosition();
+                        mOnItemClickLitener.onItemClick(holder.itemView, pos);
+                    }
+                });
+
+                holder.itemView.setOnLongClickListener(new View.OnLongClickListener()
+                {
+                    @Override
+                    public boolean onLongClick(View v)
+                    {
+                        int pos = holder.getLayoutPosition();
+                        mOnItemClickLitener.onItemLongClick(holder.itemView, pos);
+                        return false;
+                    }
+                });
+            }
         }
 
         @Override
@@ -75,6 +166,16 @@ public class ShowDemoRecyclerViewActivity extends AppCompatActivity {
                 super(view);
                 tv = (TextView) view.findViewById(R.id.id_num);
             }
+        }
+
+        public void addData(int position) {
+            mDatas.add(position, "Insert One");
+            notifyItemInserted(position);
+        }
+
+        public void removeData(int position) {
+            mDatas.remove(position);
+            notifyItemRemoved(position);
         }
     }
 
